94f70757647ea8ba740541f207100b4d0c8850d2
farhanT23 <farhan.tanvir@brainstation-23.com>
2025-07-30 15:32:08 +0600

    feat: Add CORS middleware and user not found error handling

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from fastapi import FastAPI
 from config import app_settings
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from starlette.middleware.cors import CORSMiddleware
+
+from config import BASE_PATH, app_settings
+
 from utils import lifespan
 import modules
 
@@ -9,6 +15,34 @@ app = FastAPI(lifespan=lifespan.lifespan,
               title=app_settings.app_name)
 app.include_router(modules.user.router)
 app.include_router(modules.post.router)
+
+# Allow frontend origin
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Allow frontend origin
+origins = [
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/health")
